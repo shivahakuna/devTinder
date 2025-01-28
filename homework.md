6325558365a13fbacc55903bb194a1eae1985026
Shiv Verma <vermashiva1408@gmail.com>
2025-01-29 02:12:01 +0530

    exploring get post delete

---
+++ homework.md
@@ -0,0 +1,21 @@
+S02 EP-03
+- create a repo
+- Initialize the project
+- node modules, package.json , package-lock.json
+- install express and create server
+- listen to port 7777
+- write request handlers for /test , /hello
+- install nodemon and update scripts inside package.json
+- what are dependencies
+-Difference between caret and tidle ( ^ vs ~)
+
+S02 EP-04
+-initialize git
+-.gitignore
+-create a remote repo on git
+-push all the code to remote origin
+-play with route and extension ex. /hello, / , /hello/2
+- wild card route
+-order of the routes matters a lot
+-install postman and play with it
+- write logic  to handle GET,POST,PUT,PATCH, DELETE API calls and test them in postman
--- src/app.js
+++ src/app.js
@@ -2,15 +2,27 @@ const express= require('express');
 
 const app =express();
 
-app.use("/test",(req,res)=>{
-    res.send("Hello From server")
+// app.use("/",(req,res)=>{
+//     res.send("Shiva")
+// })
+
+// app.use("/test",(req,res)=>{
+//     res.send("Hello From server")
+// })
+
+app.get("/user",(req,res)=>{
+    res.send({fname:"shiva",lname:"verma"})
 })
-app.use("/hello",(req,res)=>{
-    res.send("hey hero hello hello")
+
+app.post("/user",(req,res)=>{
+    
+    res.send("Data Saved Successfully")
 })
-app.use("/Dashboard",(req,res)=>{
-    res.send("Dashboard yha h")
+
+app.delete("/user",(req,result)=>{
+    result.send("deleted suceessfully")
 })
+
 app.listen(3000,()=>{
     console.log("My server is successfully running on port 3000");
 });
